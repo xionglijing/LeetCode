0190a5da4e31b8546b24a2a91b38060713a0845b
xionglijing <xionglijing@edsz9.com>
2020-10-28 16:23:20 +0800

    最大不重复串

--- src/com/ricoh/leetcode/xlj/LengthOfLongestSubstring.java
+++ src/com/ricoh/leetcode/xlj/LengthOfLongestSubstring.java
@@ -1,11 +1,25 @@
 package com.ricoh.leetcode.xlj;
 
+import java.util.HashMap;
+
 public class LengthOfLongestSubstring {
-    public int lengthOfLongestSubstring(String s) {
-        return 1;
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int left = 0;
+        for(int i = 0; i < s.length(); i ++){
+            if(map.containsKey(s.charAt(i))){
+                left = Math.max(left,map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i - left + 1);
+            System.out.println(map);
+        }
+        return max;
     }
     public static void main(String[] args) {
-
+        System.out.println(lengthOfLongestSubstring("ababcafjhflsakfbca"));
     }
 }
 
